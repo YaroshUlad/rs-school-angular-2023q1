8e268a0554e3ea99b8baecd7128425e03e67d5d9
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-03-13 19:22:43 +0300

    add eslint, prettier, husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rs-school-angular-2023q1';
+  title = 'rs-school-angular-2023q1'
 }
